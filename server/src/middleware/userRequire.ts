7c12f60ac7476ba59b1ec800fdaae031e4b3b602
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-05-15 13:23:11 +0200

    removed console log used for bug testing

--- server/src/middleware/userRequire.ts
+++ server/src/middleware/userRequire.ts
@@ -15,8 +15,6 @@ const userRequireMiddleware = async (req, res, next) => {
         try {
             const token = req.cookies.token;
 
-            console.log('Token:', token);
-
             if (!token) {
                 return res
                 .status(401)
